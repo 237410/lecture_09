fd739282398a75c85450ac0df12c656b02f8bd9c
jiri bartek <jiri-bartek@seznam.cz>
2022-04-11 10:58:05 +0200

    achjo

--- searching.py
+++ searching.py
@@ -30,16 +30,45 @@ def linear_search(sequence,number):
     return search_res
 
 
-
+"""
 def pattern_search(sequence, pattern):
     positions = set()
     i = 0
     while i < len(sequence) - len(pattern):
-        if sequence[i:i + len(pattern)] == pattern:
+        for letter in sequence[i:i + len(pattern)]:
+            if letter != pattern[i]:
+                break
+            else:
+                i = i + 1
+        if i == len(pattern):
             positions.add(i)
         i = i + 1
     return positions
+"""
 
+
+def binary_search(sequence, number):
+    left = 0
+    right = len(sequence) - 1
+    while left < right:
+        middle = (left + right) // 2
+        if sequence[middle] == number:
+            return middle
+        elif sequence[middle] < number:
+            left = middle + 1
+        elif sequence[middle] > number:
+            right = middle - 1
+    return None
+
+
+
+    """
+    search_res = {"positions": []}
+    for i, value in enumerate(sequence):
+        if number == value:
+            search_res["positions"].append(i)
+    return search_res
+    """
 
 
 
@@ -47,12 +76,19 @@ def pattern_search(sequence, pattern):
 def main():
     sequential_data = read_data("sequential.json","unordered_numbers")
     dna_data = read_data("sequential.json", "dna_sequence")
+    ordered_numbers = read_data("sequential.json", "ordered_numbers")
     print(sequential_data)
     print(dna_data)
+    print(ordered_numbers)
+
     results = linear_search(sequential_data, 0)
     print(results)
-    dna_results = pattern_search(dna_data,"ATA")
-    print(dna_results)
+
+    #dna_results = pattern_search(dna_data,"ATA")
+   # print(dna_results)
+
+    result = binary_search(ordered_numbers, 14)
+    print(result)
 
 
 
